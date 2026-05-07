a2ed9e79c1462c1b5c8b48c1f495fa194b872e0e
Archil3s <82018448+Archil3s@users.noreply.github.com>
2026-05-07 13:41:04 +1200

    feat: add Flutter app entry point

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'screens/home_screen.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const TextHelperApp());
+}
+
+class TextHelperApp extends StatelessWidget {
+  const TextHelperApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Text Helper',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorSchemeSeed: const Color(0xFF0A84FF),
+        brightness: Brightness.light,
+        useMaterial3: true,
+        scaffoldBackgroundColor: const Color(0xFFF2F2F7),
+        appBarTheme: const AppBarTheme(
+          centerTitle: false,
+          elevation: 0,
+          backgroundColor: Color(0xFFF2F2F7),
+          foregroundColor: CupertinoColors.label,
+        ),
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
